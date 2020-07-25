4366e2285b0c2d1875c36612a91f3c60abda80fc
Fyssion <fyssioncodes@gmail.com>
2020-07-25 17:07:09 -0500

    Add an error handler

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import telegram
 import io
 import logging
 import guesslang
+import html
 from pygments import highlight
 from pygments.lexers import get_lexer_by_name, ClassNotFound
 from pygments.formatters.img import ImageFormatter
@@ -17,6 +18,79 @@ logging.basicConfig(
 )
 
 
+class Bot(commands.Bot):
+    """Subclass that connects to a db on start and close"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.db = None
+
+    def remove_mentions(self, string):
+        string = list(string)
+
+        for i, letter in enumerate(string):
+            if letter == "@":
+                string[i] = "@\N{ZERO WIDTH JOINER}"
+
+        return "".join(string)
+
+    def get_command_signature(self, command):
+        alias = command.name
+        return "/%s %s" % (alias, command.signature)
+
+    def format_command(self, command):
+        help_text = []
+
+        signature = self.get_command_signature(command)
+        help_text.append(html.escape(f"Usage: {signature}"))
+
+        if command.description:
+            help_text.append(html.escape(command.description))
+
+        if command.help:
+            help_text.append(html.escape(command.help))
+
+        if command.examples:
+            help_text.append("")  # blank line
+            help_text.append("<b>Examples:</b>")
+
+            for example in command.examples:
+                help_text.append(html.escape(f"• /{command.name} {example}"))
+
+        return "\n".join(help_text)
+
+    def on_command_error(self, ctx, error):
+        # print("Ignoring exception in command {}:".format(ctx.command), file=sys.stderr)
+        # traceback.print_exception(
+        #     type(error), error, error.__traceback__, file=sys.stderr
+        # )
+
+        cmd = self.format_command(ctx.command)
+
+        if hasattr(ctx, "handled"):
+            return
+
+        elif isinstance(error, commands.ArgumentParsingError):
+            ctx.send(f"\N{CROSS MARK} {error}")
+
+        elif isinstance(error, commands.BadArgument):
+            formatted = self.remove_mentions(str(error))
+            ctx.send(f"\N{CROSS MARK} {html.escape(formatted)}\n\n{cmd}", parse_mode="HTML")
+
+        elif isinstance(error, commands.MissingRequiredArgument):
+            message = f"Missing a required argument: <code>{html.escape(error.param.name)}</code>"
+            ctx.send(ctx.send(f"\N{CROSS MARK} {message}\n\n{cmd}", parse_mode="HTML"))
+
+        elif isinstance(error, commands.CommandInvokeError):
+            message = (
+                "\N{WARNING SIGN} <b>Unexpected Error</b>\n\n"
+                "An unexpected error has occured:"
+                f'<pre><code class="language-python">\n{html.escape(str(error))}</code></pre>'
+                "The owner of the bot has been notified."
+            )
+            ctx.send(message, parse_mode="HTML")
+
+
 description = """
 Hi! I'm a bot that will highlight code for you.
 
@@ -24,7 +98,7 @@ Just use /code with some code, and I'll try to detect a language and highlight i
 You can optionally specify a language before the code.
 """
 
-bot = commands.Bot(token=config.token, description=description, owner_ids=[1389169565])
+bot = Bot(token=config.token, description=description, owner_ids=[1389169565])
 
 guesser = guesslang.Guess()
 
